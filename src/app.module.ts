b052fcddc59b94e6d4a8badca92505216e53e8e9
programmerShinobi <shinobiprogrammer@gmail.com>
2024-12-09 02:38:03 +0700

    [update] change code : app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Products } from './modules/product/entities/product.entity';
+import { ProductModule } from './modules/product/product.module';
+import { AuditLogs } from './modules/audit-log/entities/audit-log.entity';
+import { AuditLogModule } from './modules/audit-log/audit-log.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        entities: [Products, AuditLogs],
+        synchronize: true,
+      }),
+    }),
+    ProductModule,
+    AuditLogModule,
+  ],
 })
 export class AppModule {}
