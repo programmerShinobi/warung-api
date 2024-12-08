5de2edad3d2a986a26a1ceb1cde77a84868c75ca
programmerShinobi <shinobiprogrammer@gmail.com>
2024-12-09 02:50:33 +0700

    [update] change code : build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -f alpine.Dockerfile -t project/warung-api:1.0.0-development . && docker-compose down &&  docker-compose up -d --force-recreate
+docker build -f alpine.Dockerfile -t project/warung-api:1.0.0-production . && docker-compose down &&  docker-compose up -d --force-recreate
